62aa4fe754a3610609bf861458d31930757c9292
poseidon259 <trqhung2591@gmail.com>
2022-06-14 15:08:16 +0700

    final portf

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: [" ","Web Developer", "Web Designer", "Software Engineer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+}) 
+
+var typed2 = new Typed(".typing-2", {
+    strings: [" ","Web Developer", "Web Designer", "Software Engineer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+}) 
